d48c0f31e6968f76f0480b503b712f7f53fe8776
Dilantha Nanayakkara <450922+dilantha@users.noreply.github.com>
2024-12-06 08:19:22 +1100

    update README how to install the plugin via the BRAT plugin

--- README.md
+++ README.md
@@ -6,6 +6,17 @@ Here I'm pasting links from Firefox bookmarks and formatting them.
 
 ![Pasting and formatting](link-formatter.gif)
 
+## Installing from BRAT
+
+First install the [BRAT](https://tfthacker.com/BRAT) plugin. 
+
+Then either from the BRAT settings page, or from the command palette, select "Add beta plugin". Paste the plugin URL https://github.com/dilantha/obsidian-link-formatter and then install.
+
+## How to use
+
 Once installed select the links you want to format and search for `Link Formatter: Format links to unordered list` in the command pallet.
 
 Once you select that the links will be formatted. You can also setup a hotkey for this command.
+
+> [Link to plugin](https://github.com/dilantha/obsidian-link-formatter)
+> [Buy me a coffee](https://buymeacoffee.com/dilantha)
---
+++ bump-version.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+# Check if version type is provided
+if [ -z "$1" ]; then
+    echo "Please provide version type: major, minor, or patch"
+    exit 1
+fi
+
+# Function to increment version
+increment_version() {
+    local version=$1
+    local type=$2
+    
+    IFS='.' read -ra parts <<< "$version"
+    local major=${parts[0]}
+    local minor=${parts[1]}
+    local patch=${parts[2]}
+    
+    case $type in
+        "major")
+            major=$((major + 1))
+            minor=0
+            patch=0
+            ;;
+        "minor")
+            minor=$((minor + 1))
+            patch=0
+            ;;
+        "patch")
+            patch=$((patch + 1))
+            ;;
+        *)
+            echo "Invalid version type. Use: major, minor, or patch"
+            exit 1
+            ;;
+    esac
+    
+    echo "$major.$minor.$patch"
+}
+
+# Read current version from package.json
+current_version=$(grep '"version":' package.json | cut -d'"' -f4)
+
+# Calculate new version
+new_version=$(increment_version "$current_version" "$1")
+
+# Update package.json
+sed -i '' "s/\"version\": \"$current_version\"/\"version\": \"$new_version\"/" package.json
+
+# Update manifest.json
+sed -i '' "s/\"version\": \"$current_version\"/\"version\": \"$new_version\"/" manifest.json
+
+# Git operations
+git add package.json manifest.json
+git commit -m "chore: bump version to $new_version"
+git tag "v$new_version"
+git push origin main
+git push origin "v$new_version"
+
+echo "Successfully bumped version to $new_version and pushed changes"
